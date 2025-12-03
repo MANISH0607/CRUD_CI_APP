e8f19998887716ada7dfb96fe79f5fda5689eeb8
Manish Kumar <manishkumarsms7@gmail.com>
2025-12-03 11:40:51 +0530

    complete CRUD opretion in CI

--- application/views/edit_item.php
+++ application/views/edit_item.php
@@ -9,11 +9,11 @@
 
 <body>
 
-   
+
     <div class="jumbotron" style="background-color: lightgrey;margin: 20px; padding: 20px;">
         <h1 align="center">CRUD CI APP</h1>
     </div>
-    
+
 
     <div class="container">
 
@@ -45,15 +45,19 @@
 
 
     <!-- <?php
-    
-    print_r($singleItem);
-    
-    ?> -->
+
+            print_r($singleItem);
+
+            die;
+
+            ?> -->
 
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
 </body>
 
 </html>
